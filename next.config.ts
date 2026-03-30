30903c9ddef441ac790ed6d8869916ccc4aad488
ldbrian <ldbrian@163.com>
2026-03-30 15:24:11 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,9 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        // 告诉前端：只要遇到以 /api/ 开头的请求
         source: '/api/:path*',
-        // 就把它悄悄转发给我们的全新 Python 后端！
-        destination: 'https://api.qm-ai.pro/api/:path*', 
+        // 🌟 核心黑科技：直接写死服务器 IP 和端口，物理绕过域名备案拦截！
+        destination: 'http://123.207.43.43:8000/api/:path*', 
       },
     ];
   },
