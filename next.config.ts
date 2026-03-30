143cb1fb06a48a908768483611ff1afcafe44076
ldbrian <ldbrian@163.com>
2026-03-30 22:08:10 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         source: '/api/:path*',
         // 🌟 核心黑科技：直接写死服务器 IP 和端口，物理绕过域名备案拦截！
-        destination: 'http://123.207.43.43:8000/api/:path*', 
+        destination: 'http://123.207.43.43:8008/api/:path*', 
       },
     ];
   },
